6af050f1af431992240db4fd720240c6c6bb939b
MarcelaASAV <79098410+marcelaasenjo@users.noreply.github.com>
2024-08-28 09:58:01 -0400

    Entrega módulo 2

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Reducción barra de navegación cuando se desplaza la página
+    document.addEventListener('scroll', navbarShrink);
+
+    // Active Bootstrap scrollspy en el elemento de navegación principal
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Contraer la barra de navegación 
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activa el complemento SimpleLightbox
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
